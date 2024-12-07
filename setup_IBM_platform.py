f796cc7e6aec99492080209e3c6e5e398e7a663d
luisedseijas@gmail.com <luis.seijas@urosario.edu.co>
2024-12-07 02:03:10 -0500

    modified:   setup_IBM_platform.py

--- setup_IBM_platform.py
+++ setup_IBM_platform.py
@@ -1,4 +1,4 @@
-token = "11aa3ba1ed9053bcbb436c2f273c333b741de09122c9977700f445ca4507520eb7368d3df689f6c6eef304abeec32f51f0f4c7abcf59d72429865d9b7a0b59e6"
+token = "**********"
 
 from qiskit_ibm_runtime import QiskitRuntimeService
 
